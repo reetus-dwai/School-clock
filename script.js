56d7eb78ca50efced36183cb5a00a8f77e2cde85
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-04-05 17:51:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// while (1==1){
+function showTime(){
   let [hour, minute, second] = new Date().toLocaleTimeString("en-US").split(/:| /)
   let time = hour + ":" + minute;
   
   document.getElementById("time").innerHTML = time;
-// }
-// let date = Date.now();
-// let time = date.getHours() + ":" + date.getMinutes();
+  setTimeout(showTime, 1000);
+}
 
-// document.getElementById("time").innerHTML = time;
+showTime();
