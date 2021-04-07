cd49c0177692a79690346a1549ff78a9b69a554f
Reetus <>
2021-04-07 18:14:57 +0000

    everything

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function showTime(){
+    var date = new Date();
+    var hour = date.getHours(); // 0 - 23
+    var min = date.getMinutes(); // 0 - 59
+    var month = date.getMonth(); //0 - 11
+    var day = date.getDay();
+    var year = date.getFullYear();
+    var session = "AM";
+    
+    if(hour == 0){
+        hour = 12;
+    }
+    
+    if(hour > 12){
+        hour = hour - 12;
+        session = "PM";
+    }
 
+    month += 1;
+    
+    hour = (hour < 10) ? "0" + hour: hour;
+    min = (min  < 10) ? "0" + min: min;
+
+    var time = hour + ":" + min  + " " + session;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    
+    console.log(month + "/" + day + "/" + year)
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
