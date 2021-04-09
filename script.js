c8fe6ce200da148f5a645006e174b1a23320bfda
Reetus <>
2021-04-09 17:05:53 +0000

    Changes

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-function weak(){
+function weak(d, s, h, m){
+  var day = d;
+  var session = s;
+  var hour = h;
+  var min = m;
+  var week = "A";
+  var transitioning_to_A;
+  var transitioning_to_B;
   return "A";
-
+  if (week == "A" && day == 7 && session == "PM" && hour == 11 && min >= 50){
+    transitioning_to_B = true;
+    if (transitioning_to_A){
+      if (day == 1){
+        week = "A"
+        transitioning_to_A = false;
+        return "A"
+      }
+      return "B";
+    } else if(transitioning_to_B){
+      if (day == 1){
+        week = "B"
+        transitioning_to_B = false;
+        return "B"
+      }
+      return "A";
+    }
+  } else if (week == "B" && day == 7 && session == "PM" && hour == 11 && min >= 50){
+    transitioning_to_A = true;
+    if (transitioning_to_A){
+      if (day == 1){
+        week = "A"
+        transitioning_to_A = false;
+        return "A"
+      }
+      return "B";
+    } else if(transitioning_to_B){
+      if (day == 1){
+        week = "B"
+        transitioning_to_B = false;
+        return "B"
+      }
+      return "A";
+    }
+  }
 }
 
 function showTime(){
@@ -12,7 +53,7 @@ function showTime(){
     var day_of_the_week = date.getDay();
     var year = date.getFullYear();
     var session = "AM";
-    var week = weak();
+    var week = weak(day_of_the_week, session, hour, min);
 
     if(hour == 0){
         hour = 12;
@@ -35,7 +76,7 @@ function showTime(){
     document.getElementById("time").textContent = time + time_date;
 
     //Next period stuff
-    var next_period = "Period " + period(hour, min, session, week) + " in: " + deltaT(hour, min, period(hour, min, session, week)) + " minutes";
+    var next_period = "Period " + period(hour, min, session, week) + " in: " + deltaT(hour, min, period(hour, min, session, week));
     document.getElementById("next_period").innerText = next_period;
     document.getElementById("next_period").textContent = next_period;
 
@@ -49,7 +90,7 @@ function period(h, m, s, w){
   var session = s;
   var week = w;
 
-//A week
+  //A week
   if(week == "A"){
     if(session == "PM"){
       if(hour >= 2 && min >= 35 && hour != 12){
@@ -67,7 +108,7 @@ function period(h, m, s, w){
       }
     }
   } else {
-//B day
+  //B day
    if(session == "PM"){
       if(hour >= 2 && min >= 35 && hour != 12){
         return periods[0]; // next day's period
@@ -87,12 +128,77 @@ function period(h, m, s, w){
 }
 
 function deltaT(h, m, p){
-  var periods = ["1A", "2A", "3A", "4AB", "1B", "2B", "3B"];
   var period = p;
   var hour = h;
   var min = m;
 
-  return "some amount of ";
+  var deltaH;
+  var deltaM;
+
+  // if (period == "1A" || period == "1B") {
+  //   if (hour == 7){
+  //     deltaH = 0;
+  //     deltaM = (50 - min);
+  //   } else {
+  //     if (hour == 12){
+  //       if (min <=35){
+  //         deltaH = 7;
+  //       } else {
+  //         deltaH = 6;
+  //       }
+  //     } 
+  //   }
+  // }
+
+  if (period == "3A" || period == "3B"){
+    if (min == 0){
+      deltaH = 12 - hour;
+      deltaM = 0;
+    } else {
+      deltaH = 11 - hour;
+      deltaM = 60 - min;
+    }
+  }
+  /*
+  else if (period == "4AB"){
+    if (min == 0){
+      deltaH = 12 - hour;
+      deltaM = 0;
+    } else {
+      deltaH = 11 - hour;
+      deltaM = 60 - min;
+    }
+  }
+  */
+
+  if (deltaM == 0){
+    if (deltaH == 1){
+      return (deltaH + " hour");
+    } else {
+      return (deltaH + " hours");
+    }
+  } else if (deltaH == 0){
+    if (deltaM == 1){
+      return (deltaM + " minute");
+    } else {
+      return (deltaM + " minutes");
+    }
+  } else {
+    if (deltaH == 1){
+      if (deltaM == 1){
+        return (deltaH + " hour and " + deltaM + " minute");
+      } else {
+        return (deltaH + " hour and " + deltaM + " minutes");
+      }
+    } else { 
+      if (deltaM == 1){
+        return (deltaH + " hour and " + deltaM + " minute");
+      } else {
+        return (deltaH + " hour and " + deltaM + " minutes");
+      }
+      return (deltaH + " hours and " + deltaM + " minutes");
+    }
+  }
 }
 
 showTime();
