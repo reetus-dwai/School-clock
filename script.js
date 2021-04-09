d63eaea93c3a887468c750677d2063f2ca726579
Reetus <>
2021-04-09 01:00:19 +0000

    change

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ function showTime(){
     document.getElementById("time").innerText = time + time_date;
     document.getElementById("time").textContent = time + time_date;
 
-    var deltaT
-    var next_period = "Period " + period(hour, min) + " in: " + deltaT() + " minutes";
+    //Next period stuff
+    var next_period = "Period " + period(hour, min, session) + " in: " + deltaT(hour, min, period(hour, min, session)) + " minutes";
     document.getElementById("next_period").innerText = next_period;
     document.getElementById("next_period").textContent = next_period;
 
@@ -36,9 +36,25 @@ function showTime(){
     setTimeout(showTime, 1000);
 }
 
-function period(hour, min){
-  var periods["1A", "2A", "3A", "4AB", "1B", "2B", "3B"]
+function period(h, m, s){
+  var periods["1A", "2A", "3A", "4AB", "1B", "2B", "3B"];
+  var hour = h;
+  var min = m;
+  var session = s;
 
+  if(){
+
+  }else{
+
+  }
+  
+}
+
+function deltaT(h, m, p){
+  var periods["1A", "2A", "3A", "4AB", "1B", "2B", "3B"];
+  var period = p;
+  var hour = h;
+  var min = m;
 }
 
 showTime();
