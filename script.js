7cdee799bb2be95bcf0e1f7dcc7b694a020fb34f
Reetus <>
2021-04-09 20:19:10 +0000

    Changes

--- script.js
+++ script.js
@@ -44,6 +44,12 @@ function weak(d, s, h, m){
   }
 }
 
+function today(d){
+  var day = d;
+  var days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days_of_the_week[day];
+}
+
 function showTime(){
     var date = new Date();
     var hour = date.getHours(); // 0 - 23
@@ -56,12 +62,15 @@ function showTime(){
     var week = weak(day_of_the_week, session, hour, min);
 
     if(hour == 0){
-        hour = 12;
+      hour = 12;
+      session = "AM";
     }
     
-    if(hour > 12){
+    if(hour >= 12){
+      if (hour > 12){
         hour = hour - 12;
-        session = "PM";
+      }
+      session = "PM";
     }
 
     month += 1;
@@ -76,9 +85,13 @@ function showTime(){
     document.getElementById("time").textContent = time + time_date;
 
     //Next period stuff
-    var next_period = "Period " + period(hour, min, session, week) + " in: " + deltaT(hour, min, period(hour, min, session, week));
+    var next_period = "Period " + period(hour, min, session, week) + " in: " + deltaT(hour, min, period(hour, min, session, week, day_of_the_week), session);
     document.getElementById("next_period").innerText = next_period;
     document.getElementById("next_period").textContent = next_period;
+
+    
+    document.getElementById("day").innerText = today(day_of_the_week);
+    document.getElementById("day").textContent = today(day_of_the_week);
 
     setTimeout(showTime, 1000);
 }
@@ -88,16 +101,16 @@ function period(h, m, s, w){
   var hour = h;
   var min = m;
   var session = s;
-  var week = w;
+  var week = w;  
 
-  //A week
+  //A day
   if(week == "A"){
     if(session == "PM"){
-      if(hour >= 2 && min >= 35 && hour != 12){
+      if(hour == 1 && min >= 35 && hour != 12 || hour >= 2){
         return periods[4]; // next day's period
-      } else if (hour >= 1 && min >= 35){
-        return period[3]; // period 4
-      } else return period[2] // period 3
+      } else if (hour == 12 || hour == 1 && min <= 45){
+        return periods[3]; // period 4
+      } else return periods[2]; // period 3
     } else {
       if (hour <= 7 && min <= 50){
         return periods[0]; // 1st period
@@ -110,11 +123,11 @@ function period(h, m, s, w){
   } else {
   //B day
    if(session == "PM"){
-      if(hour >= 2 && min >= 35 && hour != 12){
+      if(hour >= 1 && min >= 35 && hour != 12){
         return periods[0]; // next day's period
-      } else if (hour >= 1 && min >= 35){
-        return period[3]; // period 4
-      } else return period[6] // period 3
+      } else if (hour >= 12){
+        return periods[3]; // period 4
+      } else return periods[6] // period 3
     } else {
       if (hour <= 7 && min <= 50){
         return periods[4]; // first period
@@ -127,45 +140,43 @@ function period(h, m, s, w){
   }
 }
 
-function deltaT(h, m, p){
+function deltaT(h, m, p, s, d){
   var period = p;
   var hour = h;
   var min = m;
+  var session = s;
+  var day_of_the_week = d;
 
   var deltaH;
   var deltaM;
 
-  // if (period == "1A" || period == "1B") {
-  //   if (hour == 7){
-  //     deltaH = 0;
-  //     deltaM = (50 - min);
-  //   } else {
-  //     if (hour == 12){
-  //       if (min <=35){
-  //         deltaH = 7;
-  //       } else {
-  //         deltaH = 6;
-  //       }
-  //     } 
-  //   }
-  // }
+  
 
 
-    /*
- else if (period == "2A" || period == "2B"){
-    if (min == 0){
-      deltaH = 12 - hour;
-      deltaM = 0;
-    } else {
-      deltaH = 11 - hour;
-      deltaM = 60 - min;
+  if (period == "1A" || period == "1B"){
+    if (session == "PM"){
+      if (min <= 50){
+        deltaH = 12 - hour + 7 + 48;
+        deltaM = 50 - min;
+      } else {
+        deltaH = 12 - hour + 6 + 48;
+        deltaM = 60 - min;
+      }
     }
-  }
-    */
-
-
-
-  if (period == "3A" || period == "3B"){
+  } else if (period == "2A" || period == "2B"){
+    if (hour == 9){
+      deltaH = 0;
+      deltaM = 35 - min;
+    } else {
+      if (min <= 35){
+        deltaM = 35 - min;
+        deltaH = 9 - hour;
+      } else {
+        deltaM = 35 + 60 - min;
+        deltaH = 8 - hour;
+      }
+    }
+  } else if (period == "3A" || period == "3B"){
     if (min == 0){
       deltaH = 12 - hour;
       deltaM = 0;
@@ -174,7 +185,7 @@ function deltaT(h, m, p){
       deltaM = 60 - min;
     }
   } else if (period == "4AB"){
-    if (min -= 45){
+    if (min <= 45){
       if (hour == 12){
         deltaH = 1
       } else {
